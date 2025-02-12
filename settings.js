780b71cb4575f86d3b169b41d733bb5a68010487
AlexRiley99 <alexriley1099@gmail.com>
2025-02-11 23:08:26 -0500

    Added settings button and began adding elements to the settings page

---
+++ settings.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const smallBook = document.getElementById("smallBook");
+    const mediumBook = document.getElementById("mediumBook");
+    const largeBook = document.getElementById("largeBook");
+
+    const red = document.getElementById("red");
+    const orange = document.getElementById("orange");
+    const yellow = document.getElementById("yellow");
+    const green = document.getElementById("green");
+    const blue = document.getElementById("blue");
+    const indigo = document.getElementById("indigo");
+    const purple = document.getElementById("purple");
+    const black = document.getElementById("black");
+    const white = document.getElementById("default");
+
+    const serif = document.getElementById("serif");
+    const sansSerif = document.getElementById("sansSerif");
+    const script = document.getElementById("script");
+    const handwriting = document.getElementById("handwriting");
+    const electronic = document.getElementById("electronic");
+
+    const preview = document.getElementById("preview");
+
+
+    //Change book size
+
+
+    //Change book color
+
+    //Change font
+});
